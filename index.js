c802af5ae95049bd895cf420d7cea1434b4abd40
Mackie <cameronfarina@gmail.com>
2019-08-25 23:47:29 -0400

    updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.json());
+app.get("/", (req, res) => res.send("Hello Worlsead"));
 app.listen(5001, () => console.log("Server is running on port 5000"));
